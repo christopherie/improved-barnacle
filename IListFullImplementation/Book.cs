11fff73e45badb0fcce45eb4930b7a20771fd5e9
christopherie <cgovenderkubiec@gmail.com>
2017-08-28 12:20:06 +0100

    IList<T> implementation

---
+++ IListFullImplementation/Book.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using static System.Console;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IListFullImplementation
+{
+    class Book : IComparable<Book>, IList<Book>
+    {
+        // properties
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public List<Book> BookList { get; } = new List<Book>();
+        public int Count => BookList.Count;
+        public bool IsReadOnly => false;
+        public Book this[int index]
+        {
+            get
+            {
+                return this[index];
+            }
+            set
+            {
+                this[index] = value;
+            }
+        }
+
+        // default constructor
+        public Book() { }
+
+        // constructor
+        public Book(string bookNo, string name, string writer, string pub, DateTime published)
+        {
+            ISBN = bookNo;
+            Title = name;
+            Author = writer;
+            Publisher = pub;
+            PublicationDate = published;
+        }
+
+        // book data
+        public override string ToString()
+        {
+            StringBuilder buffer = new StringBuilder();
+            buffer.AppendLine($"ISBN: {ISBN}");
+            buffer.AppendLine($"Title: {Title}");
+            buffer.AppendLine($"Author: {Author}");
+            buffer.AppendLine($"Publisher: {Publisher}");
+            buffer.AppendLine($"Publication date: {PublicationDate.ToShortDateString()}");
+            return buffer.ToString();
+        }
+
+        // IList add to collection
+        public void Add(Book item)
+        {
+            if (!BookList.Contains(item))
+            {
+                BookList.Add(item);
+            }
+        }
+
+        public void Contains(string v)
+        {
+            for (int i = 0; i < BookList.Count; i++)
+            {
+                if (BookList[i].ISBN == v)
+                {
+                    WriteLine("Found book");
+                }
+                else
+                {
+                    throw new Exception(message: "ISBN not found");
+                }
+            }
+        }
+
+        // IList clear collection
+        public void Clear() => BookList.Clear();
+
+        // IList check if collection already has item
+        public bool Contains(Book item)
+        {
+            bool found = false;
+            foreach (Book book in BookList)
+            {
+                if (book.Equals(item))
+                {
+                    found = true;
+                }
+            }
+            return found;
+        }
+
+        // IList copy collection to array
+        public void CopyTo(Book[] array, int arrayIndex)
+        {
+            // array can't be null
+            if (array == null)
+            {
+                throw new ArgumentNullException(message: "Array can't be null", paramName: nameof(array));
+            }
+
+            // array start index can't be negative
+            if (arrayIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(message: "Start index can't be negative", paramName: nameof(array));
+            }
+
+            // array can't have fewer elements than collection
+            if (BookList.Count > array.Length - arrayIndex + 1)
+            {
+                throw new ArgumentException(message: "Destination has fewer elements than source", paramName: nameof(array));
+            }
+
+            // all good
+            for (int i = 0; i < BookList.Count; i++)
+            {
+                array[i + arrayIndex] = BookList[i];
+            }
+        }
+
+        // IList to enable looping through list with enumerator
+        public IEnumerator<Book> GetEnumerator() => BookList.GetEnumerator();
+
+        // IList return index of item
+        public int IndexOf(Book item)
+        {
+            int index = -1;
+            for (int i = 0; i < BookList.Count; i++)
+            {
+                if (BookList[i].Equals(item))
+                {
+                    index = i;
+                    break;
+                }
+            }
+            return index;
+        }
+
+        // IList insert an item at specified index
+        public void Insert(int index, Book item)
+        {
+            // item can't be a plain Book object
+            if (item.ISBN == null)
+            {
+                throw new ArgumentNullException("Book can't be null");
+            }
+
+            // index can't be less than 0
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException("Index can't be negative");
+            }
+
+            // index can't exceed current index max
+            if (index > BookList.Count)
+            {
+                throw new ArgumentOutOfRangeException("Index can't be higher than array index maximum");
+            }
+
+            // all is good
+            BookList.Insert(index, item);
+        }
+
+        // IList remove an item
+        public bool Remove(Book item)
+        {
+            bool found = false;
+            for (int i = 0; i < BookList.Count; i++)
+            {
+                Book current = (Book)BookList[i];
+                if (current.Equals(item))
+                {
+                    found = true;
+                    BookList.RemoveAt(i);
+                    break;
+                }
+            }
+            return found;
+        }
+
+        // IList remove an item a specified index
+        public void RemoveAt(int index) => BookList.RemoveAt(index);
+
+        IEnumerator IEnumerable.GetEnumerator() => BookList.GetEnumerator();
+
+        // IComparable<Book> compare book ISBN
+        public int CompareTo(Book other) => ISBN.CompareTo(other.ISBN);
+    }
+}
---
+++ IListFullImplementation/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using static System.Console;
+using static System.Array;
+using System.Linq;
+using System.Collections;
+
+namespace IListFullImplementation
+{
+    class Program
+    {
+        // collection
+        public static Book books = new Book();
+
+        // main
+        static void Main(string[] args)
+        {
+            // Add books to collection
+            books.Add(new Book("1234-34343-5-343", "The Odyssey", "Homer", "Addison", DateTime.Parse("1888.12.12")));
+            books.Add(new Book("7532-45231-6-234", "The Iliad", "Homer", "Addison", DateTime.Parse("1890.12.12")));
+            books.Add(new Book("6235-98457-5-143", "The Roof", "Homer", "Addison", DateTime.Parse("1900.12.12")));
+
+            // Insert a book at an index
+            try
+            {
+                books.Insert(1, new Book("2365-32456-9-193", "The Cake", "Homer", "Addison", DateTime.Parse("1900.12.12")));
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+            }
+
+            // Create an array
+            Array booksArrayCopy = Array.CreateInstance(typeof(Book), books.Count);
+
+            // Convert books collection to array
+            Book[] booksArray = books.ToArray();
+
+            // Sort the array
+            Sort(booksArray);
+
+            // Copy booksArray to booksArrayCopy
+            try
+            {
+                Copy(booksArray, booksArrayCopy, booksArray.Length);
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+            }
+
+            // view the array data
+            ViewBooks(booksArrayCopy);
+
+            // check if collection contains a book with specified isbn
+            try
+            {
+                books.Contains("2365-32456-9-193");
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+            }
+
+            // remove a book at a specific index
+            books.RemoveAt(1);
+
+            // view the array data
+            WriteLine(@"After removing a book");
+            ViewBooks();
+
+            // hold
+            ReadLine();
+        }
+
+        // view books
+        public static void ViewBooks(Array array)
+        {
+            IEnumerator enumerator = array.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                WriteLine(enumerator.Current);
+                WriteLine();
+            }
+        }
+
+        // overloaded view books
+        public static void ViewBooks()
+        {
+            foreach (Book item in books)
+            {
+                WriteLine(item);
+                WriteLine();
+            }
+        }
+    }
+}
